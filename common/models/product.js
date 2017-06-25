ef5745194db12691e27791f2729df4b95241a69c
Gabesz Rózsa <gabesz.rozsa@gmail.com>
2017-06-25 12:05:37 +0200

    validation rules

--- common/models/product.js
+++ common/models/product.js
@@ -18,7 +18,55 @@ module.exports = function(Product) {
     const results = {
       status: `You bought ${quantity} product(s)`,
     };
-    // TODO
     callback(null, results);
+
   };
+
+  // NOTE: Validate minimal length of the name
+  Product.validatesLengthOf('name', {
+    min: 3,
+    message: {
+      min: 'Name should be at least 3 characters',
+    },
+  });
+
+  Product.validatesUniquenessOf('name');
+
+  const positiveInteger = /^[0-9]*$/;
+
+  const validatePositiveInteger = function(error) {
+    if (!positiveInteger.test(this.price)) {
+      error();
+    }
+  };
+
+  Product.validate('price', validatePositiveInteger, {
+    message: 'Price should be a positive integer',
+  });
+
+  function validateMinimalPrice(err, done) {
+    const price = this.price;
+
+    process.nextTick(() => {
+      const minimalPriceFromDB = 99;
+
+      if (price < minimalPriceFromDB) {
+        err();
+      }
+      done();
+    });
+  }
+
+  Product.validateAsync('price', validateMinimalPrice, {
+    message: 'Price should be higher than the minimal price in the DB'
+  });
+
+
+
+
+
+
+
+
+
 };

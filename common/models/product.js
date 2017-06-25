704478e862c3463f00fd8651b9ee472f007d215c
Gabesz Rózsa <gabesz.rozsa@gmail.com>
2017-06-25 13:00:17 +0200

    operation hooks

--- common/models/product.js
+++ common/models/product.js
@@ -2,6 +2,18 @@
 
 module.exports = function(Product) {
 
+  Product.observe('before save', function(ctx, next) {
+    if (ctx.instance && ctx.instance.categoryId) {
+      return Product.app.models.Category
+      .count({ id: ctx.instance.categoryId })
+      .then(res => {
+        if (res < 1) {
+          return Promise.reject('Error adding product to non-existing category');
+        }
+      });
+    }
+  });
+
 
   const validQuantity = quantity => Boolean(quantity > 0);
 
